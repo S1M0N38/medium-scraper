989929de4844e652c28f8e307d386ece13c562d7
S1M0N38 <bertolottosimone@gmail.com>
2020-02-21 20:50:58 +0100

    post Spider

--- medium/spiders/post_spiders.py
+++ medium/spiders/post_spiders.py
@@ -1,10 +1,11 @@
-from scrapy import Request, Spider
-from scrapy.utils.sitemap import Sitemap
+import json
+
+import scrapy
 
 from medium.items import Post
 
 
-class PostIdSpider(Spider):
+class PostIdSpider(scrapy.Spider):
 
     ''' Crawl xml sitemap looking for post_id '''
 
@@ -12,15 +13,17 @@ class PostIdSpider(Spider):
 
     def start_requests(self):
         url = 'https://medium.com/sitemap/sitemap.xml'
-        yield Request(url, self.parse_sitemap)
+        yield scrapy.Request(url, self.parse_sitemap)
 
     def parse_sitemap(self, response):
-        s = Sitemap(response.body)
+        s = scrapy.utils.sitemap.Sitemap(response.body)
 
         if s.type == 'sitemapindex':
             for loc in s:
                 if self.url_filter() in loc['loc']:
-                    yield Request(loc['loc'], callback=self.parse_sitemap)
+                    yield scrapy.Request(
+                        loc['loc'], callback=self.parse_sitemap
+                    )
         elif s.type == 'urlset':
             for loc in s:
                 post_id = loc['loc'].split('/')[-1].split('-')[-1]
@@ -37,6 +40,38 @@ class PostIdSpider(Spider):
         return url
 
 
-class PostSpider(Spider):
+class PostSpider(scrapy.Spider):
 
     ''' Exstract post data starting from post_id '''
+
+    name = 'post'
+
+    def start_requests(self):
+        self.cur.execute(
+            '''
+            SELECT post_id
+            FROM post
+            WHERE creator_id is NULL'''
+        )
+        for post_id in self.cur.fetchall():
+            url = f'https://medium.com/_/api/posts/{post_id[0]}'
+            yield scrapy.Request(url, self.parse_post)
+
+    def parse_post(self, response):
+        data = json.loads(response.text[16:])
+
+        if not data.get('success'):
+            raise scrapy.exceptions.IgnoreRequest
+
+        post = data['payload']['value']
+
+        yield Post(
+            post_id=post['id'],
+            creator_id=post['creatorId'],
+            language=post['detectedLanguage'],
+            first_published_at=post['firstPublishedAt'],
+        )
+
+        # TODO handle requets (write middleware)
+        # TODO content = post['content']
+        # TODO virtuals = post['virtuals']

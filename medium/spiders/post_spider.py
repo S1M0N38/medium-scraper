09a07228f6d28f85e0682c94c1bd3fe5b132087c
S1M0N38 <bertolottosimone@gmail.com>
2020-02-22 13:05:32 +0100

    Separate post_id and post spider

--- medium/spiders/post_spider.py
+++ medium/spiders/post_spider.py
@@ -5,41 +5,6 @@ import scrapy
 from medium.items import Post
 
 
-class PostIdSpider(scrapy.Spider):
-
-    ''' Crawl xml sitemap looking for post_id '''
-
-    name = 'post_id'
-
-    def start_requests(self):
-        url = 'https://medium.com/sitemap/sitemap.xml'
-        yield scrapy.Request(url, self.parse_sitemap)
-
-    def parse_sitemap(self, response):
-        s = scrapy.utils.sitemap.Sitemap(response.body)
-
-        if s.type == 'sitemapindex':
-            for loc in s:
-                if self.url_filter() in loc['loc']:
-                    yield scrapy.Request(
-                        loc['loc'], callback=self.parse_sitemap
-                    )
-        elif s.type == 'urlset':
-            for loc in s:
-                post_id = loc['loc'].split('/')[-1].split('-')[-1]
-                yield Post(post_id=post_id)
-
-    def url_filter(self):
-        url = '/posts/'
-        if hasattr(self, 'year'):
-            url += f'{self.year}/'
-            if hasattr(self, 'month'):
-                url += f'posts-{self.year}-{self.month}-'
-                if hasattr(self, 'day'):
-                    url += f'{self.day}'
-        return url
-
-
 class PostSpider(scrapy.Spider):
 
     ''' Exstract post data starting from post_id '''

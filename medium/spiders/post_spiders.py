60057de9cefff4671181ffcb14be86bb8c97791f
S1M0N38 <bertolottosimone@gmail.com>
2020-02-22 03:14:17 +0100

    handle 302

--- medium/spiders/post_spiders.py
+++ medium/spiders/post_spiders.py
@@ -45,33 +45,46 @@ class PostSpider(scrapy.Spider):
     ''' Exstract post data starting from post_id '''
 
     name = 'post'
+    handle_httpstatus_list = [302]
 
     def start_requests(self):
         self.cur.execute(
             '''
             SELECT post_id
             FROM post
-            WHERE creator_id is NULL'''
+            WHERE available is NULL'''
         )
         for post_id in self.cur.fetchall():
             url = f'https://medium.com/_/api/posts/{post_id[0]}'
             yield scrapy.Request(url, self.parse_post)
 
     def parse_post(self, response):
+        code = response.status
+
+        if code == 200:
+            yield self._post_200(response)
+
+        elif code == 302:
+            yield self._post_302(response)
+
+        # add here other requests code if necessary
+
+    def _post_200(self, response):
         data = json.loads(response.text[16:])
-
-        if not data.get('success'):
-            raise scrapy.exceptions.IgnoreRequest
-
         post = data['payload']['value']
 
-        yield Post(
+        return Post(
             post_id=post['id'],
+            available=1,
             creator_id=post['creatorId'],
             language=post['detectedLanguage'],
             first_published_at=post['firstPublishedAt'],
         )
 
-        # TODO handle requets (write middleware)
-        # TODO content = post['content']
-        # TODO virtuals = post['virtuals']
+    def _post_302(self, response):
+        post_id = response.url.split('/')[-1]
+        self.logger.debug('The post {post_id} removed (user is blacklisted)')
+        return Post(post_id=post_id, available=0)
+
+    # TODO content = post['content']
+    # TODO virtuals = post['virtuals']
